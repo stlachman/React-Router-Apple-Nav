eb9b0acd1db316002a16f6142d5ec41aff06f924
Seth Lachman <lachmanseth17@gmail.com>
2019-05-06 16:54:55 -0400

    Created application and removed boilerplate code (service workers). Added react-router and wrapped app in router component.

---
+++ apple-nav/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h2>Hello</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
